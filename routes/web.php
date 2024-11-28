12594159f316b5005e8021c361273d956765d5ac
Elvis David Quinteros Siles <dquinteros630@gmail.com>
2024-11-28 17:47:36 -0400

    generacion del plan de dieta en el sistema

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,28 @@
-    <?php
+<?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AssignController;
 use App\Http\Controllers\ConsultationController;
+use App\Http\Controllers\ContactoController;
+use App\Http\Controllers\EjercicioController;
+use App\Http\Controllers\EjercicioEquipoController;
+use App\Http\Controllers\EjercicioMusculoController;
+use App\Http\Controllers\EquipoController;
+use App\Http\Controllers\MuscleController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\RutinaController;
+use App\Http\Controllers\DietController;
+use App\Http\Controllers\TipoEjercicioController;
 use App\Livewire\ChatMessage;
 use App\Livewire\Pages\AssistantPage;
 use App\Livewire\Pages\Chat;
-use Illuminate\Support\Facades\Route;
 use App\Livewire\FormularioProgreso;
+use App\Models\EjercicioEquipo;
+use App\Models\EjercicioMusculo;
+
+
+
 
     /*
     |--------------------------------------------------------------------------
@@ -31,19 +45,32 @@ use App\Livewire\FormularioProgreso;
         Route::get('/chat', function () {
             $chat = auth()->user()->conversations()->create([]);
 
-        return redirect()->route('chat.show', $chat);
-    })->name('chat');
-    Route::get('/chat/{conversation:uuid}', Chat::class)->name('chat.show');
-    Route::get('/assistant', AssistantPage::class);
+            return redirect()->route('chat.show', $chat);
+        })->name('chat');
+        // modulo de administracon
+        Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+        Route::resource('/roles', RoleController::class)->names('roles');
+        Route::resource('/permissions', PermissionController::class)->names('permissions');
+        Route::resource('/assign', AssignController::class)->names('assign');
+        Route::get('/recomendacionIA', [ConsultationController::class, 'recomendacionIA'])->name('recomendacionIA');
+        Route::get('/generar-plan', [ConsultationController::class,'generarPlan'])->name('generarPlan');
+        Route::resource('/consultation', ConsultationController::class)->names('consultation');
+        Route::get('/formulario', FormularioProgreso::class)->name('formulario.inicio');
+        Route::get('/diet/{id}', [DietController::class, 'getDietDetails']);
+        Route::get('/diet', [DietController::class, 'index'])->name('diet');
 
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::resource('/roles', RoleController::class)->names('roles');
-    Route::resource('/permissions', PermissionController::class)->names('permissions');
-    Route::resource('/assign', AssignController::class)->names('assign');
-    Route::resource('/consultation', ConsultationController::class)->names('consultation');
+        //modulo de gestion de equipo
+        Route::get('/formulario', FormularioProgreso::class)->name('formulario.inicio');
+        Route::resource('/muscle',MuscleController::class)->names('muscle');
+        Route::resource('/equipo',EquipoController::class)->names('equipo');
+        Route::resource('/tipoEjercicio',TipoEjercicioController::class)->names('tipoEjercicio');
+        Route::resource('/ejercicio',EjercicioController::class)->names('ejercicio');
+        Route::resource('/ejercicioMuscle',EjercicioMusculoController::class)->names('ejercicioMuscle');
+        Route::resource('/ejercicioEquipo',EjercicioEquipoController::class)->names('ejercicioEquipo');
+        Route::resource('/rutina',RutinaController::class)->names('rutina');
+        Route::resource('/contacto',ContactoController::class)->names('contacto');
 
-    Route::get('/formulario', FormularioProgreso::class)->name('formulario.inicio');
-});
+    });
 
 
 
